6c7cba3d5a5aa166bd5f17517195fba0f08d9e27
jinDeHao <o.idhmaid@gmail.com>
2024-05-01 18:07:27 +0100

    push pusha

--- e-shop/src/App.tsx
+++ e-shop/src/App.tsx
@@ -1,12 +1,15 @@
 import PreNav1 from "./components/PreNav1";
 import PreNav2 from "./components/PreNav2";
+import NavBar from "./components/NavBar";
 import './styles/PreNav1.css'
 import './styles/PreNav2.css'
+import './styles/NavBar.css'
 
 const App = () =>
   <div>
     <PreNav1 />
     <PreNav2 />
+    <NavBar />
   </div>;
 
 export default App;
