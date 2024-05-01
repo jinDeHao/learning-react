b874fa4b17384e35e9db721cd784202f69655240
jinDeHao <o.idhmaid@gmail.com>
2024-05-01 17:46:24 +0100

    pushy pusha

---
+++ e-shop/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+const NavBar = () =>
+    <div className="NavBar">
+        <nav>
+            <ul>
+                <li>All Categories</li>
+                <li>Products</li>
+                <li>Blog</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        <nav>
+            <ul>
+                <li>LIMITED SALE <img src="" alt="" /></li>
+                <li>Best Seller</li>
+                <li>New Arrival</li>
+            </ul>
+        </nav>
+    </div>
+
+export default NavBar
