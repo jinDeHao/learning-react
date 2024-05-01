3b8b027bdc98c77aaf918f62d15c0423d4bfe38b
H-Asmaa <asmaehadar32@gmail.com>
2024-05-01 19:21:43 +0100

    Committing new component, the hero section.

--- e-shop/src/App.tsx
+++ e-shop/src/App.tsx
@@ -1,12 +1,15 @@
 import PreNav1 from "./components/PreNav1";
 import PreNav2 from "./components/PreNav2";
+import Hero from "./components/Hero";
 import './styles/PreNav1.css'
 import './styles/PreNav2.css'
+import './styles/Hero.css'
 
 const App = () =>
   <div>
     <PreNav1 />
     <PreNav2 />
+    <Hero />
   </div>;
 
 export default App;
